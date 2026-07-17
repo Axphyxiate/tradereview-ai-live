d42af13665f307583d52317b59ce841834d129d9
Axphyxiate <Axphyxiate@users.noreply.github.com>
2026-07-16 23:59:13 -0500

    Use verified compatible Supabase anon key

--- app/supabase-config.js
+++ app/supabase-config.js
@@ -1,4 +1,4 @@
 window.TRADE_REVIEW_SUPABASE_CONFIG = Object.freeze({
   url: "https://ujzvtwygejditdkyagcl.supabase.co",
-  publishableKey: "sb_publishable_GXHlMs5M-V6yKsnX1BlICQ_F7Ee2qDU"
+  publishableKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqenZ0d3lnZWpkaXRka3lhZ2NsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyNTEyNDksImV4cCI6MjA5OTgyNzI0OX0.SFMZiObuD9pLV1eDgm5h8bnzFlKgOOme2qau2z6nN7Y"
 });
